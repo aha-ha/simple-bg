be8a61290e9a8fa88a825b5111bc1d297cd9d0bf
Aha Ha <ahawespe@gmail.com>
2026-03-24 09:52:12 +0100

    added URL parameter function 'seconds' to choose weather to display seconds or not. off by default

--- script.js
+++ script.js
@@ -3,7 +3,16 @@ function updateTime() {
     const now = new Date();
     const hours = String(now.getHours()).padStart(2, '0');
     const minutes = String(now.getMinutes()).padStart(2, '0');
-    span.textContent = `${hours}:${minutes}`;
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    if (urlParameter('seconds') == true) {
+        span.textContent = `${hours}:${minutes}:${seconds}`;
+    } else {
+        span.textContent = `${hours}:${minutes}`;
+    }
+}
+function urlParameter(name) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(name);
 }
 setInterval(updateTime, 1000);
 updateTime();
